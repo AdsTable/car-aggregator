9a107a6109232ca4ac14e3e27f305d324df363c2
ronek22 <jronek3010@gmail.com>
2020-11-13 02:38:11 +0100

    Added endpoints for counting available values for fields

--- backend/cars/urls.py
+++ backend/cars/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from cars.views import OfferListView, OfferRetrieveView
+from cars.views import OfferListView, OfferRetrieveView, count_available_fields,available_models_for_brand 
 
 urlpatterns = [
     path('list', OfferListView.as_view(), name='offer-list'),
-    path('<int:pk>', OfferRetrieveView.as_view())
+    path('<int:pk>', OfferRetrieveView.as_view()),
+    path('map', count_available_fields, name='map-fields'),
+    path('map/<str:brand>', available_models_for_brand, name='map-brand')
 ]
--- backend/cars/views.py
+++ backend/cars/views.py
@@ -4,6 +4,11 @@ from rest_framework import generics
 from cars.models import Offer
 import django_filters.rest_framework
 from rest_framework.filters import OrderingFilter
+from django.db.models import Count
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.db.models import F
+
 
 class OfferListView(generics.ListAPIView):
     queryset = Offer.objects.all()
@@ -16,4 +21,36 @@ class OfferListView(generics.ListAPIView):
 class OfferRetrieveView(generics.RetrieveAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferItemSerializer
+
+def get_available_options_for_field(field):
+    return list(Offer.objects.values(value=F(field)).filter(value__isnull=False).annotate(count=Count('value')).order_by("value"))
+
+def get_models_for_brand(brand):
+    return list(Offer.objects.filter(brand=brand).values(value=F('model')).annotate(count=Count('value')))
+
+@api_view(['GET'])
+def count_available_fields(request):
+    data = {
+        'brand': get_available_options_for_field('brand'),
+        'fuel': get_available_options_for_field('fuel'),
+        'primary_damage': get_available_options_for_field('primary_damage'),
+        'secondary_damage': get_available_options_for_field('secondary_damage'),
+        'body_style': get_available_options_for_field('body_style'),
+        'transmission': get_available_options_for_field('transmission'),
+        'drive': get_available_options_for_field('drive'),
+        'production_year': get_available_options_for_field('production_year')
+    }
+
+    return Response(data=data)
+
+@api_view(['GET'])
+def available_models_for_brand(request, brand):
+    return Response(get_models_for_brand(brand))
+
+
+
+
+
+
+
     

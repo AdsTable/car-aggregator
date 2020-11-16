15e3b7dce6d83fe834c0b63d62ac38622e92526e
ronek22 <jronek3010@gmail.com>
2020-11-16 07:09:37 +0100

    Edited django admin

--- backend/cars/admin.py
+++ backend/cars/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Offer
 
 # Register your models here.
+@admin.register(Offer)
 class CarsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('offerId', 'brand', 'model', 'vin', 'production_year', 'sold', 'sale_date', 'created_at', 'updated_at')
+    list_filter = ('sold', 'sale_date', 'created_at')
+    search_fields = ('brand', 'model')
+    date_hierarchy = 'sale_date'
 
-admin.site.register(Offer, CarsAdmin)
+    

8092f5432dea9737a3b736fc6ecf0a5763dfad39
ronek22 <jronek3010@gmail.com>
2020-11-20 15:58:34 +0100

    Stylized home component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { OfferComponent } from './offer/offer.component';
 
 const routes: Routes = [
   {
-    path: 'home', component: HomeComponent
+    path: '', component: HomeComponent
   },
   {
     path: 'offers', component: OfferComponent, data: {
@@ -18,7 +18,7 @@ const routes: Routes = [
   },
   { path: 'offer/:id', component: OfferDetailsComponent },
 
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({

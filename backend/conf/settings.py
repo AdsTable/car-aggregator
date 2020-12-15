4698e78b905975a9ba056a0a45435d005b1a6a3e
ronek22 <jronek3010@gmail.com>
2020-12-15 08:06:16 +0100

    Change cache mechanism to redis

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -122,6 +122,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
 
 
 STATIC_URL = '/static/'
